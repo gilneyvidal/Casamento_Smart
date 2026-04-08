75fd7495a9ae28f443d464a431999f97ce2834ec
gw.vidal <gw.vidal@gmail.com>
2026-04-08 11:47:05 -0300

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Pega as chaves que voce colocou no arquivo .env
-const supabaseUrl = process.env.NEXT_https://cotbgkybknxujcffsmxy.supabase.co;
-const supabaseKey = process.env.NEXT_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvdGJna3lia254dWpjZmZzbXh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NDY1MTUsImV4cCI6MjA5MTIyMjUxNX0.OJge9KLKxnDYAwZtB1QqYruQ1G9nS-btSGacXjfXuS0;
+// Aqui o codigo apenas "chama" os nomes que estao no arquivo .env
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Cria o mensageiro que vai levar e trazer dados do banco
+// Verifica se as chaves existem antes de conectar
+if (!supabaseUrl || !supabaseKey) {
+  console.error("ERRO: Chaves do Supabase nao encontradas no arquivo .env");
+}
+
+// Cria o cliente de conexao
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log("Conexao com o banco de dados preparada!");
+console.log("Conexao com o banco de dados configurada!");
